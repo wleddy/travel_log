078cafeda13f0a6e0a999ac06d233b235fa4496a
Bill Leddy <bill@williesworkshop.net>
2025-10-30 15:00:24 -0700

    Add arrival_state_of_charge col and rename state_of_charge to departure_state_of_charge

--- models.py
+++ models.py
@@ -32,7 +32,7 @@ class LogEntry(SqliteTable):
             'lat' REAL,
             'lng' REAL,
             'odometer' INT,
-            'state_of_charge' INT,
+            'departure_state_of_charge' INT,
             'cost' REAL,
             'trip_id' INTEGER REFERENCES trip(id) ON DELETE CASCADE
             """
@@ -46,9 +46,9 @@ class LogEntry(SqliteTable):
     
         column_list = []
         
-        # column_list = [
-        #     {'name':'fuel_added','definition':'REAL',}
-        # ]
+        column_list = [
+            {'name':'arrival_state_of_charge','definition':'REAL',}
+        ]
         
         return column_list
     
